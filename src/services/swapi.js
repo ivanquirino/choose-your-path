06a1795b10b39c9f1ae2a96c3856b6a726c3b89f
Ivan Quirino <ivanquirino1928@gmail.com>
2020-09-20 00:48:22 -0300

    change to https

--- src/services/swapi.js
+++ src/services/swapi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getPeopleById = (id) => {
   return axios
-    .get(`https://swapi.dev/api/people/${id}`)
+    .get(`http://swapi.dev/api/people/${id}`)
     .then((res) => res.data);
 };
 
